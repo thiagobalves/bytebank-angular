6ffe62b2b0ddcbb10570596861db0852b93d8e0e
Thiago Alves <thiago.balves@hotmail.com>
2022-04-23 18:46:37 -0300

    inclusão de funções para realizar transferência e limpar campos de dados

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'bytebank';
+
+  transferencia: any;
+
+  transferir($event) {
+    this.transferencia = $event;
+
+    console.log('event', $event);
+  }
+
 }
